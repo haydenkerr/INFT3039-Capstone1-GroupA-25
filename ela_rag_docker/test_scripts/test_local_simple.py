b7623c502b6e3d3c362240583e08b7644369fae7
Hayden Kerr <hayden.kerr@gmail.com>
2025-04-08 20:26:57 +1000

    udpated readme file

--- ela_rag_docker/test_scripts/test_local_simple.py
+++ ela_rag_docker/test_scripts/test_local_simple.py
@@ -2,9 +2,9 @@ import requests
 import pandas as pd
 
 # load test data set
-# host_port = "192.168.1.17:8001"
+host_port = "192.168.1.17:8001"
 # Docker
-host_port = "192.168.1.17:8002"
+# host_port = "192.168.1.17:8002"
 # Define the GitHub raw CSV URL
 csv_url_test = "https://github.com/haydenkerr/INFT3039-Capstone1-GroupA-25/raw/refs/heads/main/datasets/processed_dataset2_test_data.csv"
 
@@ -16,7 +16,7 @@ df_test = df_test[['prompt', 'essay', 'band', 'cleaned_evaluation','Task Achieve
 df_test.rename(columns={'prompt':'question'}, inplace=True)
 
 # Example test case
-question_id = 98
+question_id = 152
 # word wrap the text output below  
 
 
