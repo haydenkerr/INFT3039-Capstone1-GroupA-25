f06ac7c6cc3a08bf3f8920026775c426f1254a25
Hayden Kerr <hayden.kerr@gmail.com>
2025-05-06 23:02:00 +1000

    Update host_port configuration and simplify URL construction in test script

--- ela_rag_docker/test_scripts/test_local_simple.py
+++ ela_rag_docker/test_scripts/test_local_simple.py
@@ -2,10 +2,10 @@ import requests
 import pandas as pd
 
 # load test data set
-# host_port = "3.107.188.95:8002"
+host_port = "http://"+"3.27.223.201:8002"
 # Docker
-host_port = "http://"+"192.168.1.17:8001"
-host_port = "http://"+"127.0.0.1:8001"
+# host_port = "http://"+"192.168.1.17:8001"
+# host_port = "http://"+"127.0.0.1:8001"
 # Define the GitHub raw CSV URL
 csv_url_test = "https://github.com/haydenkerr/INFT3039-Capstone1-GroupA-25/raw/refs/heads/main/datasets/processed_dataset2_test_data.csv"
 
@@ -73,7 +73,7 @@ print(responseGet.json())
 # test results return from database
 tracking_id = "3e19f672-76aa-4fc5-ace6-f25a836713cd"
 responseGet = requests.get(
-    "http://"+host_port+"/results/"+tracking_id,
+    host_port+"/results/"+tracking_id,
     headers={"x-api-key": API_KEY}
     
 )
